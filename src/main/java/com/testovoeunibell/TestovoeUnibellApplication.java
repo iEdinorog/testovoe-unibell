e6263fbac02cda21bb67937b3ed4242a55125b7f
iEdinorog <glebslayer@gmail.com>
2024-09-04 17:54:52 +0500

    init

---
+++ src/main/java/com/testovoeunibell/TestovoeUnibellApplication.java
@@ -0,0 +1,13 @@
+package com.testovoeunibell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestovoeUnibellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestovoeUnibellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testovoeunibell/TestovoeUnibellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testovoeunibell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestovoeUnibellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
